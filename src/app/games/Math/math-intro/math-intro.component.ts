e38611df218f2979635ec3ba5706c3ba3b7cf781
Tzruya Dror <drortz123@gmail.com>
2021-01-04 11:57:35 +0200

    adding color list

--- src/app/games/Math/math-intro/math-intro.component.ts
+++ src/app/games/Math/math-intro/math-intro.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+declare var $: any;
 @Component({
   selector: 'app-math-intro',
   templateUrl: './math-intro.component.html',
   styleUrls: ['./math-intro.component.css']
 })
-export class MathIntroComponent implements OnInit {
 
+export class MathIntroComponent implements OnInit {
+  
   constructor(public router: Router) { }
 
 
